bb160bc4d32261a4a56b693a86ed96f4c720e8e7
Wojciech Waldon <wigiels14@gmail.com>
2017-10-27 12:08:09 +0200

    Project scratch

---
+++ src/main/java/pl/wojciechwaldon/bpsas/BusinessPartnerSearchAssistanceSystemApplication.java
@@ -0,0 +1,12 @@
+package pl.wojciechwaldon.bpsas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessPartnerSearchAssistanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessPartnerSearchAssistanceSystemApplication.class, args);
+	}
+}
---
+++ src/main/java/pl/wojciechwaldon/bpsas/ServletInitializer.java
@@ -0,0 +1,13 @@
+package pl.wojciechwaldon.bpsas;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BusinessPartnerSearchAssistanceSystemApplication.class);
+	}
+
+}
